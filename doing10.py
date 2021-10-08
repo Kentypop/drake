8f6fd24195efbfcc544196a1944c5320a888c8c0
kentypop <Levimcc7@hotmail.com>
2021-10-08 17:20:16 +0900

    ubuntu now aye

--- doing10.py
+++ doing10.py
@@ -1,12 +1,12 @@
-def has_33(nums):
+def paper_doll(text):
+	added= ''
 
-	for i in range(0, len(nums)-1):
-		print(i)
-		if nums[i:i+2] == [3,3]:
-			return True  
-
-	return False
+	for f in text:
+		two_copy = f*3
+		added= added+two_copy
 
 
+	return added
 
-print(has_33([1,3,4,3,3]))
+
+print(paper_doll("Hello"))
---
+++ section6_50_practiceexercises8.py
@@ -0,0 +1,52 @@
+# https://github.com/Pierian-Data/Complete-Python-3-Bootcamp/blob/master/03-Methods%20and%20Functions/03-Function%20Practice%20Exercises.ipynb
+
+"""
+LEVEL 2 PROBLEMS
+
+PAPER DOLL: Given a string, 
+return a string where for every character in the original there are three characters
+"""
+
+def paper_doll(text):
+	added= ''
+
+	for f in text:
+		#two_copy = f*2
+		#added= f+two_copy
+		print(f)
+
+		added= added.join(f*3)
+		print(added)
+		print("@@@@@")
+
+	return added
+
+
+print(paper_doll("aHello"))
+
+
+def paper_doll(text):
+	added= ''
+
+	for f in text:
+		two_copy = f*2
+		added= f+two_copy
+
+
+	return added
+
+
+print(paper_doll("Hello"))
+
+
+
+
+# SOLUTION
+
+def paper_doll(text):
+    result = ''
+    for char in text:
+        result += char * 3
+    return result
+
+paper_doll('Hello')
