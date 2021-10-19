9dfc14c1c22a44c08ce58801b537bc24f9992c3f
seant <Levimcc7@hotmail.com>
2021-10-19 21:09:04 +0900

    yuh yuh

--- doing11.py
+++ doing11.py
@@ -1,7 +1,131 @@
+import random
+
+
+
 def display_board(board):
 
-    print(board[1] + "|" + str(board[2:4]) ) 
+    print(board[1] + "|" + board[2] + "|" + board[3])
+    print(board[4] + "|" + board[5] + "|" + board[6])
+    print(board[7] + "|" + board[8] + "|" + board[9])
 
 
-test_board = ['#','X','O','X','O','X','O','X','O','X']
+test_board = ['#','X','','','X','','','X','','']
+
+
+
+def player_input():
+
+	choice= "wrong"
+
+	print("Welcome to Tic Tac Toe!")
+
+	#Repeat till its X or O
+	while choice not in ['X', 'O']:
+
+		choice= input("Player : Do you want to be X or O?")
+
+		if choice not in ['X', 'O']:
+			print("Sorry, its invalid.")
+
+	return choice
+
+
+
+def place_marker(board, marker, position):
+
+
+	while position not in list(range(1,11)):
+
+		position= int(input("Choose a position from 1-10. "))
+
+		if choice not in list(range(1,11)):
+
+			print("Invalid, choose again from 1-10")
+
+
+	board[position]= marker
+
+	return board
+
+
+
+#place_marker(test_board,'$',8)
 display_board(test_board)
+
+
+
+def win_check(board, mark):
+    
+    if board[1] == mark and board[2] == mark and board[3] == mark:
+
+    	return True
+
+    elif board[4] == mark and board[5] == mark and board[6] == mark:
+
+    	return True
+
+    elif board[7] == mark and board[8] == mark and board[9] == mark:
+
+    	return True
+
+    elif board[2] == mark and board[5] == mark and board[8] == mark:
+
+    	return True
+
+    elif board[3] == mark and board[6] == mark and board[9] == mark:
+
+    	return True
+
+    elif board[1] == mark and board[4] == mark and board[7] == mark:
+
+    	return True
+
+    elif board[1] == mark and board[5] == mark and board[9] == mark:
+
+    	return True
+
+    elif board[3] == mark and board[5] == mark and board[7] == mark:
+
+    	return True
+
+    else:
+    	print("hmmm")
+
+
+
+def choose_first():
+    
+    num= random.randint(1,2)
+
+    if num == 1:
+    	print("player1 goes first")
+    elif num == 2:
+    	print("player2 goess first")
+    else:
+    	print("??????????")
+
+
+choose_first()
+
+
+def space_check(board, position):
+
+	return board[position] == ''
+
+
+
+def full_board_check(board):
+
+	for i in range(1,10):
+		if space_check(board, i):
+			return False
+
+	#BOARD IS  FULL IF WE RETURN True
+	return True
+
+
+
+
+
+    
+    
--- doing12.py
+++ doing12.py
@@ -1,20 +1,34 @@
+def display_board(board):
 
-
-num = 7
-
-if num> 1:
-	mylist= []
-
-	#loops all numbers and counts withint the numbers given
-	for f in range(2,num+1):
-
-		if num%f == 0:
-			print(f"this {num} not prime number")
-		else:
-			print(mylist.append(f))
+    print(board[1] + "|" + board[2] + "|" + board[3])
+    print(board[4] + "|" + board[5] + "|" + board[6])
+    print(board[7] + "|" + board[8] + "|" + board[9])
 
 
 
+def space_check(board, position):
+
+	return board[position] == ''
 
 
 
+def full_board_check(board):
+
+	for i in range(1,10):
+		if space_check(board, i):
+			return False
+
+	#BOARD IS  FULL IF WE RETURN True
+	return True
+
+
+
+test_board = ['#','','','','','','','','','']
+
+
+
+#print(space_check(test_board, 2))
+
+print("@@@@@@@@@")
+
+print(full_board_check(test_board))
